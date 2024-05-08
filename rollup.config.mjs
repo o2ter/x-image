92f2cca641f6edc382ac695f033b3c5257576cf1
Susan Cheng <susan.doggie@gmail.com>
2024-05-08 23:19:46 +0800

    init

---
+++ rollup.config.mjs
@@ -0,0 +1,72 @@
+import _ from 'lodash';
+import resolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
+import commonjs from '@rollup/plugin-commonjs';
+import babel from '@rollup/plugin-babel';
+import json from '@rollup/plugin-json';
+import dts from 'rollup-plugin-dts';
+
+const rollupPlugins = [
+  typescript({ declaration: false }),
+  babel({
+    babelrc: false,
+    exclude: 'node_modules/**',
+    babelHelpers: 'bundled',
+  }),
+  commonjs({
+    transformMixedEsModules: true,
+  }),
+  json(),
+];
+
+const rollupConfig = {
+  input: 'src/index',
+  external: [
+    /node_modules/,
+  ],
+};
+
+export default [
+  ...['.node', ''].map(suffix => ({
+    ...rollupConfig,
+    output: [
+      {
+        file: `dist/index${suffix}.js`,
+        format: 'cjs',
+        sourcemap: true,
+      },
+      {
+        file: `dist/index${suffix}.mjs`,
+        format: 'esm',
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      resolve({
+        extensions: [
+          ..._.uniq([suffix, '']).flatMap(x => [`${x}.tsx`, `${x}.jsx`]),
+          ..._.uniq([suffix, '']).flatMap(x => [`${x}.ts`, `${x}.mjs`, `${x}.js`]),
+        ]
+      }),
+      ...rollupPlugins
+    ],
+  })),
+  ...['.node', ''].map(suffix => ({
+    ...rollupConfig,
+    output: [
+      {
+        file: `dist/index${suffix}.d.ts`,
+        format: 'es',
+      },
+    ],
+    plugins: [
+      resolve({
+        extensions: [
+          ..._.uniq([suffix, '']).flatMap(x => [`${x}.tsx`, `${x}.jsx`]),
+          ..._.uniq([suffix, '']).flatMap(x => [`${x}.ts`, `${x}.mjs`, `${x}.js`]),
+        ]
+      }),
+      dts()
+    ],
+  })),
+];
