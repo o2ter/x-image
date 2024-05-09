51cfb9e4bddbd9fb7a65ec1947fa79a12eb6d1d7
Susan Cheng <susan.doggie@gmail.com>
2024-05-10 00:02:08 +0800

    Update index.js

--- test/index.js
+++ test/index.js
@@ -5,3 +5,9 @@ const sharp = require('sharp');
 const test = image(sharp(path.resolve(__dirname, 'test.png')))
 
 console.log(test)
+
+console.log(test.toJimp())
+
+console.log(test.toMirada())
+
+console.log(test.toSharp())
